633fa40d4c785d63a74d36d72dc8020f2ae6f6b1
androiddevelopergame <androiddevelopergame@gmail.com>
2026-08-20 17:02:52 +0300

    Add files via upload

---
+++ Stable-Diff.sh
@@ -0,0 +1,8 @@
+pip install pygit2==1.15.1\
+cd content
+rm -r /content/Stable-Diffusion-webui/
+git clone https://github.com/androiddevelopergame/Stable-Diffusion-webui.git Stable-Diffusion-webui
+
+
+cd /content/Stable-Diffusion-webui
+pip install -r requirements_versions.txt
